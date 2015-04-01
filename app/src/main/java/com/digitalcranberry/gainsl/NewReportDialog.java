d2fa7e7bfa12395703be006650675e82d05b1b56
Yochannah Yehudi <yochannah@gmail.com>
2015-04-01 20:36:07 +0100

    Uses GSON to convert message to json, and grabs content text from the dialog

--- app/src/main/java/com/digitalcranberry/gainsl/NewReportDialog.java
+++ app/src/main/java/com/digitalcranberry/gainsl/NewReportDialog.java
@@ -4,35 +4,49 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.digitalcranberry.gainsl.model.Report;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link NewReportDialog.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link NewReportDialog#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class NewReportDialog extends DialogFragment {
+    private ImageButton cameraButton;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    String mCurrentPhotoPath;
+    ImageView mImageView;
+    private Report report;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.fragment_new_report_dialog, null))
+        final View fragView = inflater.inflate(R.layout.fragment_new_report_dialog, null);
+        builder.setView(fragView)
                 .setPositiveButton(R.string.send, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // FIRE ZE Report!
+                        generateReportDetails(fragView);
+                        SendReport.uploadReport(report);
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -41,6 +55,73 @@ public class NewReportDialog extends DialogFragment {
                     }
                 });
         // Create the AlertDialog object and return it
+       //TODO: WHY THIS LINE NO WORKY? cameraButton = (ImageButton) findViewById(R.id.new_report_button);
         return builder.create();
     }
+
+    public void addPhotoClickListener() {
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                dispatchTakePictureIntent();
+            }
+
+        });
+
+    }
+
+    private void generateReportDetails(View view) {
+
+        EditText content = (EditText) view.findViewById(R.id.input_report_description);
+        report = new Report(content.getText().toString());
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Log.wtf("Oh no", "problem creating image file");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "Gains_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
 }
--- app/src/main/java/com/digitalcranberry/gainsl/SendReport.java
+++ app/src/main/java/com/digitalcranberry/gainsl/SendReport.java
@@ -1,9 +1,25 @@
 package com.digitalcranberry.gainsl;
 
+import android.util.Log;
+
+import com.digitalcranberry.gainsl.model.Report;
+import com.google.gson.Gson;
+
 /**
  * Created by yo on 01/04/15.
  */
 public class SendReport {
     //Upload image method
+
+    private static Gson gson = new Gson();
+
+    public static String uploadImage() {
+        return ""; //will eventually return the blobstore id
+    }
     //upload report method
+    public static String uploadReport(Report report) {
+        String reportString = gson.toJson(report);
+        Log.i("GAINSL",reportString);
+        return reportString;
+    }
 }
